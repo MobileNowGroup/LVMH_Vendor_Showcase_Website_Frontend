822a2dcdeeba1f1c614004dc746fd4864db5b212
cuiyuan <charlescy1031@icloud.com>
2023-09-07 17:28:09 +0800

    update deploy shell

--- .deploy/shell/run.dev.sh
+++ .deploy/shell/run.dev.sh
@@ -4,8 +4,8 @@ npm run build:dev
 
 sh .deploy/shell/docker-app.sh
 docker -v
-export 
-  BUILD_IMG_TAG=mobilenowdev/lvmh_vendor_showcase_frontend:dev\
+export REGISTRY_REPO=registry.cn-hangzhou.aliyuncs.com\
+  BUILD_IMG_TAG=mobilenowdev/lvmh_vendor_showcase_frontend:dev-1.0\
   BUILD_ENV_DOCKER_FILE=Dockerfile.dev\
 
 sh .deploy/shell/build-image.sh
